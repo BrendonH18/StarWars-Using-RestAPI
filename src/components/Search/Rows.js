6ed9372530cddd73c1e7153999bf9953d013eb7a
SunShark3 <hall.brendonp@gmail.com>
2021-09-22 17:14:48 -0400

    It Works!!

--- src/components/Search/Rows.js
+++ src/components/Search/Rows.js
@@ -16,10 +16,7 @@ const DisplaySearchRows = ({ searchResults, keys }) => {
     let displayObjectPiecePreparation = {}
 
     // promises array
-    let promises = []
-    
-    // not sure yet
-    const valueOfObjectPieceMultipleNamesArray = ['Hello World', ]
+    let promises = []    
 
     // look at each key/value pair, decide how to handle it, and handle it (build promises)
     for (let j = 0; j < keys.length; j++) {
@@ -38,65 +35,44 @@ const DisplaySearchRows = ({ searchResults, keys }) => {
 
       // handle empty arrays - (OK)
       if (isArrayLengthZero) {
-        // displayObjectPiecePreparation[keys[j]] = ""
+        displayObjectPiecePreparation[keys[j]] = ""
       }
 
       // handle strings - (OK)
       if (!isArray && !isHTTP) {
-        // displayObjectPiecePreparation[keys[j]] = valueOfObjectPiece
+        displayObjectPiecePreparation[keys[j]] = valueOfObjectPiece
       }
 
       // handle single http request - (OK)
       if (isHTTP) {
-        // promises.push(
-        //   newPromise(valueOfObjectPiece, displayObjectPiecePreparation, keys[j])
-        // )
+        promises.push(
+          newPromise(valueOfObjectPiece, displayObjectPiecePreparation, keys[j])
+        )
       }
 
       // handle multiple http requests
       if (isArray && !isArrayLengthZero) {
-        
-        let count = 1
-
-        let newList = []
-
+        let multipleNamesArray = []
         valueOfObjectPiece.forEach(HTTPRequest => { 
-          let piece
           promises.push(
-            
-            // newPromise(HTTPRequest, displayObjectPiecePreparation, keys[j], valueOfObjectPieceMultipleNamesArray, valueOfObjectPiece, count)
-
-            newPromise(HTTPRequest, displayObjectPiecePreparation, keys[j], ['piece'], valueOfObjectPiece, count)
-
-          )
-          // console.log("External :")
-
-          // update end list
-          // newList = [...newList, piece] 
-          count = count + 1
+            newPromise(HTTPRequest, displayObjectPiecePreparation, keys[j], true).then(results => {
+              multipleNamesArray = [...multipleNamesArray, results]
+              displayObjectPiecePreparation[keys[j]] = multipleNamesArray.join(", ")
+          })
+          )        
         });
-
-        
-      
-      
-      
         // put back into object
-        // displayObjectPiecePreparation[keys[j]] = ["--Multiple--", valueOfObjectPieceMultipleNamesArray, valueOfObjectPiece]
+        displayObjectPiecePreparation[keys[j]] = multipleNamesArray
       }
-      // check object so far
-      // console.log(`Internal ${i+1}.${j+1} - `, displayObjectPiecePreparation)
-      
-      // check promises
-      // console.log("promises: ", promises)
     }
     // check promises
-    console.log("promises: ", promises)
+    // console.log("promises: ", promises)
 
     // resolve promises
     Promise.allSettled(promises).catch(err => console.log(err))
 
     // check object again
-    console.log(`Done ${i+1} - `, displayObjectPiecePreparation)
+    // console.log(`Done ${i+1} - `, displayObjectPiecePreparation)
 
     // push object to displayFull
     displayObjectFull.push(displayObjectPiecePreparation)
@@ -107,73 +83,25 @@ const DisplaySearchRows = ({ searchResults, keys }) => {
 
   // displayObjectFull is complete and ready to be passed to the component below
 
-  async  function newPromise(HTTPRequest, object, key, nameArray = null, originalArray, count) {
+  async  function newPromise(HTTPRequest, object, key, nameArray = null) {
     if (nameArray === null) return await new Promise((resolve, reject) => {
       axios.get(HTTPRequest).then(response => object[key] = response.data.name).catch(err => console.log(err))
     })
-    if (nameArray !== null) return await new Promise((resolve, reject) => {
-      axios.get(HTTPRequest).then(response => {
+    if (nameArray !== null)  {
+      let resolveResponse = ''
+      await axios.get(HTTPRequest).then((response) => {
         
-        // My goal is to pull out the name data
-        setNameArrayState(response.data.name)
-
-        console.log("Internal: ", response.data.name);
-        
-        // object[key] = 
-        
-        // formatArray(object[key], response.data.name, originalArray, count)
-      
-      }).catch(err => console.log(err))
-  })
+        // Switch to handle error
+        response.data['name'] === undefined ? resolveResponse = response.data['title'] : resolveResponse = response.data['name']
+      })
+      return Promise.resolve((resolveResponse))
+    }
+  
 }
 
   function formatArray(nameArray, newElement, originalArray, count) {
     return ["--Multiple--", count, newElement, originalArray, nameArray]
   }
-
-
-      // for (let k = 0; k < keys.length; k++) {
-      //   const element = searchResults[i][keys[k]];
-      //   let isArrayLengthZero, isArrayFirstElementHTTP
-      //   let isArray = Array.isArray(element)
-      //   if(isArray) isArrayLengthZero = element.length === 0
-      //   if(!isArrayLengthZero) isArrayFirstElementHTTP = element[0].includes("https")
-      //   let isHTTP = element.includes("https")
-      //   if (isHTTP) {
-
-      //     // add one promise to list
-      //     // be sure to update in last .then "attributeResponse[keys[k]] = element"
-      //     // console.log("element: ", element)
-      //     promises.push(
-      //        newPromise(element, displayObjectPiecePreparation, keys[k])
-      //      )
-      //   }
-      //   if (isArray && !isArrayLengthZero) {
-
-      //     // add multiple promises to list
-      //     // be sure to update in last .then "attributeResponse[keys[k]] = element"
-      //     // element.forEach(HTTPRequest => {
-      //     //   promises.push(
-      //     //     newPromise(HTTPRequest, attributeResponse, keys[k], nameArray).then()
-      //     //   )
-      //     // });
-
-      //     // console.log("name Array: ", nameArray)
-      //   }
-      //   if (!isArray && !isHTTP) {
-      //     displayObjectPiecePreparation[keys[k]] = element
-      //   }
-      //   if (isArrayLengthZero) {
-      //     displayObjectPiecePreparation[keys[k]] = ""
-      //   }
-        
-
-      // }
-      // Create Promise.all chain
-      // Use .then until the chain is finished
-      
-
-
   return(
     <>
 
